5a87c1b9a7451003f87dd536cb3557c8500f4ba4
Khashayar  Haghighi <khaghig1@my.westga.edu>
2026-04-10 14:05:54 -0500

    Created lookup models

---
+++ KLS-Furniture/Model/Lookups/IdNameLookupItem.cs
@@ -0,0 +1,16 @@
+﻿namespace KLS_Furniture.Model.Lookups
+{
+    /// <summary>
+    /// Generic lookup row for combo boxes.
+    /// </summary>
+    public class IdNameLookupItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ KLS-Furniture/Model/Lookups/RentalMemberLookupItem.cs
@@ -0,0 +1,16 @@
+﻿namespace KLS_Furniture.Model.Lookups
+{
+    /// <summary>
+    /// Lightweight member lookup item used in the rental screen.
+    /// </summary>
+    public class RentalMemberLookupItem
+    {
+        public int MemberId { get; set; }
+        public string DisplayText { get; set; } = "";
+
+        public override string ToString()
+        {
+            return DisplayText;
+        }
+    }
+}
