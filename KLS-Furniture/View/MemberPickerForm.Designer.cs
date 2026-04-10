6c24df210b640ca7a0be214390352b29cc984e01
Khashayar  Haghighi <khaghig1@my.westga.edu>
2026-04-10 14:07:39 -0500

    Created MemberPickerForm

---
+++ KLS-Furniture/View/MemberPickerForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace KLS_Furniture.View
+{
+    partial class MemberPickerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvMembers = new System.Windows.Forms.DataGridView();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.colMemberId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDisplayText = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMembers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMembers
+            // 
+            this.dgvMembers.AllowUserToAddRows = false;
+            this.dgvMembers.AllowUserToDeleteRows = false;
+            this.dgvMembers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMembers.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colMemberId,
+            this.colDisplayText});
+            this.dgvMembers.Location = new System.Drawing.Point(50, 55);
+            this.dgvMembers.MultiSelect = false;
+            this.dgvMembers.Name = "dgvMembers";
+            this.dgvMembers.ReadOnly = true;
+            this.dgvMembers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvMembers.Size = new System.Drawing.Size(240, 150);
+            this.dgvMembers.TabIndex = 0;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(50, 229);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(215, 229);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // colMemberId
+            // 
+            this.colMemberId.HeaderText = "Member ID";
+            this.colMemberId.Name = "colMemberId";
+            this.colMemberId.ReadOnly = true;
+            // 
+            // colDisplayText
+            // 
+            this.colDisplayText.HeaderText = "DisplayText";
+            this.colDisplayText.Name = "colDisplayText";
+            this.colDisplayText.ReadOnly = true;
+            // 
+            // MemberPickerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 304);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.dgvMembers);
+            this.Name = "MemberPickerForm";
+            this.Text = "MemberPickerForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMembers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvMembers;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colMemberId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDisplayText;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ KLS-Furniture/View/MemberPickerForm.cs
@@ -0,0 +1,51 @@
+﻿using KLS_Furniture.Model.Lookups;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace KLS_Furniture.View
+{
+    public partial class MemberPickerForm : Form
+    {
+        public RentalMemberLookupItem SelectedMember { get; private set; }
+
+        public MemberPickerForm(List<RentalMemberLookupItem> members)
+        {
+            InitializeComponent();
+
+            dgvMembers.AutoGenerateColumns = false;
+            dgvMembers.DataSource = members;
+
+            btnSelect.Click += BtnSelect_Click;
+            btnCancel.Click += BtnCancel_Click;
+        }
+
+        private void BtnSelect_Click(object sender, EventArgs e)
+        {
+            if (dgvMembers.CurrentRow == null)
+            {
+                MessageBox.Show("Please select a member first.",
+                    "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            SelectedMember = dgvMembers.CurrentRow.DataBoundItem as RentalMemberLookupItem;
+
+            if (SelectedMember == null)
+            {
+                MessageBox.Show("Please select a valid member.",
+                    "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
