5ecb0c5d003ddcbf2ba7787e5177d71c09fd52cb
Khashayar  Haghighi <khaghig1@my.westga.edu>
2026-04-12 11:44:08 -0500

    Fixed design issues

--- KLS-Furniture/UserControls/FurnitureRentalUserControl.Designer.cs
+++ KLS-Furniture/UserControls/FurnitureRentalUserControl.Designer.cs
@@ -52,6 +52,9 @@
             this.lblTotalCostValue = new System.Windows.Forms.Label();
             this.dtpDueDate = new System.Windows.Forms.DateTimePicker();
             this.btnConfirmRental = new System.Windows.Forms.Button();
+            this.MemberDetailsLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.grpSelectedMember.SuspendLayout();
             this.grpFurnitureSearch.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvFurnitureResults)).BeginInit();
@@ -63,18 +66,18 @@
             // grpSelectedMember
             // 
             this.grpSelectedMember.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.grpSelectedMember.Controls.Add(this.MemberDetailsLabel);
             this.grpSelectedMember.Controls.Add(this.btnFindMember);
             this.grpSelectedMember.Controls.Add(this.lblSelectedMemberValue);
-            this.grpSelectedMember.Location = new System.Drawing.Point(6, 33);
+            this.grpSelectedMember.Location = new System.Drawing.Point(6, 3);
             this.grpSelectedMember.Name = "grpSelectedMember";
             this.grpSelectedMember.Size = new System.Drawing.Size(582, 100);
             this.grpSelectedMember.TabIndex = 0;
             this.grpSelectedMember.TabStop = false;
-            this.grpSelectedMember.Text = "Selected Member";
             // 
             // btnFindMember
             // 
-            this.btnFindMember.Location = new System.Drawing.Point(475, 43);
+            this.btnFindMember.Location = new System.Drawing.Point(472, 40);
             this.btnFindMember.Name = "btnFindMember";
             this.btnFindMember.Size = new System.Drawing.Size(92, 23);
             this.btnFindMember.TabIndex = 1;
@@ -84,7 +87,7 @@
             // lblSelectedMemberValue
             // 
             this.lblSelectedMemberValue.AutoSize = true;
-            this.lblSelectedMemberValue.Location = new System.Drawing.Point(25, 43);
+            this.lblSelectedMemberValue.Location = new System.Drawing.Point(25, 45);
             this.lblSelectedMemberValue.Name = "lblSelectedMemberValue";
             this.lblSelectedMemberValue.Size = new System.Drawing.Size(104, 13);
             this.lblSelectedMemberValue.TabIndex = 0;
@@ -94,6 +97,7 @@
             // 
             this.grpFurnitureSearch.BackColor = System.Drawing.SystemColors.ControlLight;
             this.grpFurnitureSearch.Controls.Add(this.dgvFurnitureResults);
+            this.grpFurnitureSearch.Controls.Add(this.label1);
             this.grpFurnitureSearch.Controls.Add(this.btnSearch);
             this.grpFurnitureSearch.Controls.Add(this.txtFurnitureId);
             this.grpFurnitureSearch.Controls.Add(this.lblFurnitureId);
@@ -101,28 +105,27 @@
             this.grpFurnitureSearch.Controls.Add(this.cboStyle);
             this.grpFurnitureSearch.Controls.Add(this.cboCategory);
             this.grpFurnitureSearch.Controls.Add(this.lblCategory);
-            this.grpFurnitureSearch.Location = new System.Drawing.Point(6, 164);
+            this.grpFurnitureSearch.Location = new System.Drawing.Point(6, 109);
             this.grpFurnitureSearch.Name = "grpFurnitureSearch";
-            this.grpFurnitureSearch.Size = new System.Drawing.Size(582, 201);
+            this.grpFurnitureSearch.Size = new System.Drawing.Size(582, 256);
             this.grpFurnitureSearch.TabIndex = 0;
             this.grpFurnitureSearch.TabStop = false;
-            this.grpFurnitureSearch.Text = "Furniture Search";
             // 
             // dgvFurnitureResults
             // 
             this.dgvFurnitureResults.AllowUserToAddRows = false;
             this.dgvFurnitureResults.AllowUserToDeleteRows = false;
             this.dgvFurnitureResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvFurnitureResults.Location = new System.Drawing.Point(22, 78);
+            this.dgvFurnitureResults.Location = new System.Drawing.Point(18, 86);
             this.dgvFurnitureResults.Name = "dgvFurnitureResults";
             this.dgvFurnitureResults.ReadOnly = true;
             this.dgvFurnitureResults.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvFurnitureResults.Size = new System.Drawing.Size(545, 106);
+            this.dgvFurnitureResults.Size = new System.Drawing.Size(545, 147);
             this.dgvFurnitureResults.TabIndex = 7;
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(492, 29);
+            this.btnSearch.Location = new System.Drawing.Point(492, 43);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(75, 23);
             this.btnSearch.TabIndex = 6;
@@ -131,7 +134,7 @@
             // 
             // txtFurnitureId
             // 
-            this.txtFurnitureId.Location = new System.Drawing.Point(428, 29);
+            this.txtFurnitureId.Location = new System.Drawing.Point(428, 43);
             this.txtFurnitureId.Name = "txtFurnitureId";
             this.txtFurnitureId.Size = new System.Drawing.Size(58, 20);
             this.txtFurnitureId.TabIndex = 5;
@@ -139,7 +142,7 @@
             // lblFurnitureId
             // 
             this.lblFurnitureId.AutoSize = true;
-            this.lblFurnitureId.Location = new System.Drawing.Point(360, 33);
+            this.lblFurnitureId.Location = new System.Drawing.Point(360, 47);
             this.lblFurnitureId.Name = "lblFurnitureId";
             this.lblFurnitureId.Size = new System.Drawing.Size(62, 13);
             this.lblFurnitureId.TabIndex = 4;
@@ -148,7 +151,7 @@
             // lblStyle
             // 
             this.lblStyle.AutoSize = true;
-            this.lblStyle.Location = new System.Drawing.Point(197, 32);
+            this.lblStyle.Location = new System.Drawing.Point(197, 46);
             this.lblStyle.Name = "lblStyle";
             this.lblStyle.Size = new System.Drawing.Size(30, 13);
             this.lblStyle.TabIndex = 3;
@@ -157,7 +160,7 @@
             // cboStyle
             // 
             this.cboStyle.FormattingEnabled = true;
-            this.cboStyle.Location = new System.Drawing.Point(233, 28);
+            this.cboStyle.Location = new System.Drawing.Point(233, 42);
             this.cboStyle.Name = "cboStyle";
             this.cboStyle.Size = new System.Drawing.Size(121, 21);
             this.cboStyle.TabIndex = 2;
@@ -165,7 +168,7 @@
             // cboCategory
             // 
             this.cboCategory.FormattingEnabled = true;
-            this.cboCategory.Location = new System.Drawing.Point(70, 28);
+            this.cboCategory.Location = new System.Drawing.Point(70, 42);
             this.cboCategory.Name = "cboCategory";
             this.cboCategory.Size = new System.Drawing.Size(121, 21);
             this.cboCategory.TabIndex = 1;
@@ -173,7 +176,7 @@
             // lblCategory
             // 
             this.lblCategory.AutoSize = true;
-            this.lblCategory.Location = new System.Drawing.Point(15, 32);
+            this.lblCategory.Location = new System.Drawing.Point(15, 46);
             this.lblCategory.Name = "lblCategory";
             this.lblCategory.Size = new System.Drawing.Size(49, 13);
             this.lblCategory.TabIndex = 0;
@@ -182,18 +185,18 @@
             // grpShoppingCart
             // 
             this.grpShoppingCart.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.grpShoppingCart.Controls.Add(this.label2);
             this.grpShoppingCart.Controls.Add(this.dgvCart);
             this.grpShoppingCart.Controls.Add(this.btnRemoveItem);
             this.grpShoppingCart.Controls.Add(this.btnUpdateQty);
             this.grpShoppingCart.Controls.Add(this.btnAddToCart);
             this.grpShoppingCart.Controls.Add(this.nudQuantity);
             this.grpShoppingCart.Controls.Add(this.lblSelectedItemQty);
-            this.grpShoppingCart.Location = new System.Drawing.Point(6, 395);
+            this.grpShoppingCart.Location = new System.Drawing.Point(6, 371);
             this.grpShoppingCart.Name = "grpShoppingCart";
-            this.grpShoppingCart.Size = new System.Drawing.Size(582, 200);
+            this.grpShoppingCart.Size = new System.Drawing.Size(582, 224);
             this.grpShoppingCart.TabIndex = 1;
             this.grpShoppingCart.TabStop = false;
-            this.grpShoppingCart.Text = "Shopping Cart";
             // 
             // dgvCart
             // 
@@ -203,12 +206,12 @@
             this.dgvCart.Location = new System.Drawing.Point(22, 76);
             this.dgvCart.Name = "dgvCart";
             this.dgvCart.ReadOnly = true;
-            this.dgvCart.Size = new System.Drawing.Size(545, 93);
+            this.dgvCart.Size = new System.Drawing.Size(545, 126);
             this.dgvCart.TabIndex = 5;
             // 
             // btnRemoveItem
             // 
-            this.btnRemoveItem.Location = new System.Drawing.Point(492, 32);
+            this.btnRemoveItem.Location = new System.Drawing.Point(492, 38);
             this.btnRemoveItem.Name = "btnRemoveItem";
             this.btnRemoveItem.Size = new System.Drawing.Size(75, 23);
             this.btnRemoveItem.TabIndex = 4;
@@ -217,7 +220,7 @@
             // 
             // btnUpdateQty
             // 
-            this.btnUpdateQty.Location = new System.Drawing.Point(398, 32);
+            this.btnUpdateQty.Location = new System.Drawing.Point(398, 38);
             this.btnUpdateQty.Name = "btnUpdateQty";
             this.btnUpdateQty.Size = new System.Drawing.Size(75, 23);
             this.btnUpdateQty.TabIndex = 3;
@@ -226,7 +229,7 @@
             // 
             // btnAddToCart
             // 
-            this.btnAddToCart.Location = new System.Drawing.Point(299, 32);
+            this.btnAddToCart.Location = new System.Drawing.Point(299, 38);
             this.btnAddToCart.Name = "btnAddToCart";
             this.btnAddToCart.Size = new System.Drawing.Size(75, 23);
             this.btnAddToCart.TabIndex = 2;
@@ -235,7 +238,7 @@
             // 
             // nudQuantity
             // 
-            this.nudQuantity.Location = new System.Drawing.Point(144, 35);
+            this.nudQuantity.Location = new System.Drawing.Point(144, 39);
             this.nudQuantity.Name = "nudQuantity";
             this.nudQuantity.Size = new System.Drawing.Size(120, 20);
             this.nudQuantity.TabIndex = 1;
@@ -243,7 +246,7 @@
             // lblSelectedItemQty
             // 
             this.lblSelectedItemQty.AutoSize = true;
-            this.lblSelectedItemQty.Location = new System.Drawing.Point(25, 37);
+            this.lblSelectedItemQty.Location = new System.Drawing.Point(25, 42);
             this.lblSelectedItemQty.Name = "lblSelectedItemQty";
             this.lblSelectedItemQty.Size = new System.Drawing.Size(91, 13);
             this.lblSelectedItemQty.TabIndex = 0;
@@ -252,7 +255,7 @@
             // lblDueDate
             // 
             this.lblDueDate.AutoSize = true;
-            this.lblDueDate.Location = new System.Drawing.Point(41, 613);
+            this.lblDueDate.Location = new System.Drawing.Point(31, 612);
             this.lblDueDate.Name = "lblDueDate";
             this.lblDueDate.Size = new System.Drawing.Size(53, 13);
             this.lblDueDate.TabIndex = 2;
@@ -261,7 +264,7 @@
             // lblTotalCost
             // 
             this.lblTotalCost.AutoSize = true;
-            this.lblTotalCost.Location = new System.Drawing.Point(337, 611);
+            this.lblTotalCost.Location = new System.Drawing.Point(326, 611);
             this.lblTotalCost.Name = "lblTotalCost";
             this.lblTotalCost.Size = new System.Drawing.Size(55, 13);
             this.lblTotalCost.TabIndex = 3;
@@ -270,7 +273,7 @@
             // lblTotalCostValue
             // 
             this.lblTotalCostValue.AutoSize = true;
-            this.lblTotalCostValue.Location = new System.Drawing.Point(398, 612);
+            this.lblTotalCostValue.Location = new System.Drawing.Point(387, 612);
             this.lblTotalCostValue.Name = "lblTotalCostValue";
             this.lblTotalCostValue.Size = new System.Drawing.Size(37, 13);
             this.lblTotalCostValue.TabIndex = 4;
@@ -278,19 +281,55 @@
             // 
             // dtpDueDate
             // 
-            this.dtpDueDate.Location = new System.Drawing.Point(115, 609);
+            this.dtpDueDate.Location = new System.Drawing.Point(104, 609);
             this.dtpDueDate.Name = "dtpDueDate";
             this.dtpDueDate.Size = new System.Drawing.Size(200, 20);
             this.dtpDueDate.TabIndex = 5;
             // 
             // btnConfirmRental
             // 
-            this.btnConfirmRental.Location = new System.Drawing.Point(469, 608);
+            this.btnConfirmRental.Location = new System.Drawing.Point(458, 608);
             this.btnConfirmRental.Name = "btnConfirmRental";
             this.btnConfirmRental.Size = new System.Drawing.Size(101, 23);
             this.btnConfirmRental.TabIndex = 6;
             this.btnConfirmRental.Text = "Confirm Rental";
             this.btnConfirmRental.UseVisualStyleBackColor = true;
+            // 
+            // MemberDetailsLabel
+            // 
+            this.MemberDetailsLabel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.MemberDetailsLabel.AutoSize = true;
+            this.MemberDetailsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MemberDetailsLabel.Location = new System.Drawing.Point(19, 16);
+            this.MemberDetailsLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.MemberDetailsLabel.Name = "MemberDetailsLabel";
+            this.MemberDetailsLabel.Size = new System.Drawing.Size(133, 17);
+            this.MemberDetailsLabel.TabIndex = 2;
+            this.MemberDetailsLabel.Text = "Selected Member";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Furniture Search";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(19, 16);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Shopping Cart";
             // 
             // FurnitureRentalUserControl
             // 
@@ -346,5 +385,8 @@
         private System.Windows.Forms.Label lblTotalCostValue;
         private System.Windows.Forms.DateTimePicker dtpDueDate;
         private System.Windows.Forms.Button btnConfirmRental;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label MemberDetailsLabel;
+        private System.Windows.Forms.Label label1;
     }
 }
