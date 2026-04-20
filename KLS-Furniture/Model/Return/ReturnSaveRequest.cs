c2dd65a572256737ffdd1f2fb21ae80d91b8e7fc
Sergei Pozdeev <122926581+reking06@users.noreply.github.com>
2026-04-20 11:44:34 -0400

    add return helper models for save flow

---
+++ KLS-Furniture/Model/Return/ReturnSaveRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KLS_Furniture.Model.Return
+{
+    /// <summary>
+    /// Represents the data needed to save one return transaction.
+    /// </summary>
+    public class ReturnSaveRequest
+    {
+        public int EmployeeId { get; set; }
+        public DateTime ReturnDateTime { get; set; }
+        public List<ReturnItemInput> Items { get; set; } = new List<ReturnItemInput>();
+    }
+}
