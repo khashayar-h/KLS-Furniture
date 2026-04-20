bbec7321750a82ee52ce9d04e009a8531fffb385
Sergei Pozdeev <122926581+reking06@users.noreply.github.com>
2026-04-20 13:31:17 -0400

    verify return save flow with smoke test

--- KLS-Furniture/Program.cs
+++ KLS-Furniture/Program.cs
@@ -1,7 +1,6 @@
 ﻿using KLS_Furniture;
 using KLS_Furniture.Controller;
 using KLS_Furniture.DAL;
-using KLS_Furniture.DebugTools;
 using KLS_Furniture.View;
 using System;
 using System.Threading;
@@ -28,6 +27,9 @@ namespace KLSFurniture
             {
                 /// smoke test for rental save functionality, can be removed after testing
                 /// RentalDebugRunner.RunRentalSaveSmokeTest();
+                /// 
+                /// smoke test for Return functionality, can be removed after testing
+                /// ReturnDebugRunner.RunReturnSmokeTest();
 
                 EmployeeDBDAL employeeDBDAL = new EmployeeDBDAL();
                 AuthController authController = new AuthController(employeeDBDAL);
@@ -45,7 +47,7 @@ namespace KLSFurniture
                         Application.Run(new MainForm(loginForm, authController));
                     }
                 }
-                
+
             }
             catch (Exception ex)
             {
