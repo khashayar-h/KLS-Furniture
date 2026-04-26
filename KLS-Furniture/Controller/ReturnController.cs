1b43a58134eff1ca4f439db08239fd3dea558480
Khashayar  Haghighi <khaghig1@my.westga.edu>
2026-04-26 11:40:20 -0500

    Created Return Controller

---
+++ KLS-Furniture/Controller/ReturnController.cs
@@ -0,0 +1,35 @@
+﻿using KLS_Furniture.DAL;
+using KLS_Furniture.Model.Lookups;
+using KLS_Furniture.Model.Return;
+using System.Collections.Generic;
+
+namespace KLS_Furniture.Controller
+{
+    /// <summary>
+    /// Handles return UI requests and forwards them to the DAL.
+    /// </summary>
+    public class ReturnController
+    {
+        private readonly ReturnDBDAL _returnDbDal;
+
+        public ReturnController()
+        {
+            _returnDbDal = new ReturnDBDAL();
+        }
+
+        public List<ReturnRentalItemLookup> GetReturnableRentalItemsForMember(int memberId)
+        {
+            return _returnDbDal.GetReturnableRentalItemsForMember(memberId);
+        }
+
+        public ReturnSaveResult SaveReturnTransaction(ReturnSaveRequest request)
+        {
+            return _returnDbDal.SaveReturnTransaction(request);
+        }
+
+        public List<ReturnHistoryItem> GetReturnReceiptItems(int returnTransactionId)
+        {
+            return _returnDbDal.GetReturnReceiptItems(returnTransactionId);
+        }
+    }
+}
